8da6a3c35e46ee8c41fedc8640c7b719604114bd
Bryan Ang <bryan.ang>
2023-11-13 17:08:51 -0800

    api services for kanbas modules and courses

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ import express from 'express';
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./Modules/routes.js";
 
 const app = express()
 app.use(cors());
 app.use(express.json());
 
+ModuleRoutes(app);
+CourseRoutes(app);
 Lab5(app)
 Hello(app)
 app.listen(4000)
---
+++ courses/routes.js
@@ -0,0 +1,56 @@
+import Database from "../Database/index.js";
+
+function CourseRoutes(app) {
+
+    // GET all courses
+    app.get("/api/courses", (req, res) => {
+        const courses = Database.courses;
+        res.send(courses);
+    });
+
+    // GET one course by id
+    app.get("/api/courses/:id", (req, res) => {
+        const { id } = req.params;
+        console.log("courses: " + JSON.stringify(Database.courses))
+        console.log("id = " + id)
+        const course = Database.courses.find((course) => course._id === id);
+        if (!course) {
+            res.status(404).send("Course not found");
+            return;
+        }
+        res.json(course);
+    });
+
+    //POST new course
+    app.post("/api/courses", (req, res) => {
+        const course = {
+            ...req.body,
+            number: "RS" + Math.floor(Math.random() * 9999).toString(),
+            _id: new Date().getTime().toString()
+        };
+        Database.courses.push(course);
+        res.json(course);
+    });
+
+    //DELETE a course
+    app.delete("/api/courses/:id", (req, res) => {
+        const { id } = req.params;
+        Database.courses = Database.courses
+            .filter((c) => c._id !== id);
+        res.sendStatus(204);
+    });
+
+    //PUT edit a course
+    app.put("/api/courses/:id", (req, res) => {
+        const { id } = req.params;
+        const course = req.body;
+        Database.courses = Database.courses.map((c) =>
+            c._id === id ? { c, ...course } : c
+        );
+        res.sendStatus(204);
+    });
+
+
+}
+
+export default CourseRoutes;
