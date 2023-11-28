ea18e5809579cc38b418d2f1600999900a9e5444
Bryan Ang <bryan.ang>
2023-11-28 01:09:38 -0800

    enable cors origin to allow multiple URLS, dotenv var now supports multiple urls, spearated by whitespace

--- app.js
+++ app.js
@@ -22,22 +22,19 @@ const app = express()
 // if (process.env.FRONTEND_URL.includes(req.headers.origin)) {
 //     res.setHeader('Access-Control-Allow-Origin', origin);
 // }
-var origin = "";
 app.use((req, res, next) => {
-    // if (process.env.FRONTEND_URL.includes(origin)) {
-    //     origin = req.headers.origin;
-    // } else {
-    //     origin = "www.example.com"
-    // }
-    // console.log("origin: " + origin);
-    origin = req.headers.origin;
+    let origins = process.env.FRONTEND_URL.split(" ");
+    if (origins.includes(req.headers.origin)) {
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    } 
+    // origin = req.headers.origin;
     return next();
 });
-// console.log("origin: " + origin);
+// console.log(`process.env.FRONTEND_URL.split(" "): ${process.env.FRONTEND_URL.split(" ")}`)
 app.use(
     cors({
         credentials: true, // support cookies
-        origin: process.env.FRONTEND_URL,
+        origin: "",
     })
 );
 const sessionOptions = {
