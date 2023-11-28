d2b37108d34ef2d05028be7e20bdc422975ba6b4
Bryan Ang <bryan.ang>
2023-11-28 01:29:14 -0800

    re-enable multiple cors origin with callback function

--- app.js
+++ app.js
@@ -34,7 +34,16 @@ const app = express()
 app.use(
     cors({
         credentials: true, // support cookies
-        origin: process.env.FRONTEND_URL,
+        origin: function (origin, callback) {
+            // bypass the requests with no origin (like curl requests, mobile apps, etc )
+            if (!origin) return callback(null, true);
+
+            if (process.env.FRONTEND_URL.split(" ").indexOf(origin) === -1) {
+                var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        }
     })
 );
 const sessionOptions = {
